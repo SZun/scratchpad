cf8f5acf2a71ca3733b16b44471b4b5a807e08eb
Sam G. Zun <zunsamuel97@gmail.com>
2018-05-10 11:59:17 -0500

    Finished?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,33 @@
 
 
 
-
-
 $(document).ready(function(){
 
 
 
-$("#beginBtn").on("click", function(){
-
-
-$("#start").css('display','none')
-
-$("#content").css('display','inherit')
-
-$('body').css('background','#F9F9F9')
-
-var promise = document.querySelector('video').play();
-
-if (promise !== undefined) {
-  promise.then(_ => {
-    // Autoplay started!
-  }).catch(error => {
-    // Autoplay was prevented.
-    // Show a "Play" button so that user can start playback.
-  });
-}
-
-
-
-})
-
-
-})
+  $("#beginBtn").on("click", function(){
+  
+  
+  $("#start").css('display','none')
+  
+  $("#content").css('display','inherit')
+  
+  $('body').css('background','#F9F9F9')
+  
+  var promise = document.querySelector('video').play();
+  
+  if (promise !== undefined) {
+    promise.then(_ => {
+      // Autoplay started!
+    }).catch(error => {
+      // Autoplay was prevented.
+      // Show a "Play" button so that user can start playback.
+    });
+  }
+  
+  
+  
+  })
+  
+  
+  })
