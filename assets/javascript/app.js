e17dfd6b0779a03b1ed1fee52b2b4bb232652d3d
Sam G. Zun <zunsamuel97@gmail.com>
2018-05-21 16:19:07 -0500

    FreeCodeCamp Twitch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,58 @@
+/* */
+$(document).ready(function () {
 
+	var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+	var onlineIcon = "<div class='glyphicon glyphicon-ok ml-5 mt-1' style='font-size: 20px'></div>"
+	var offlineIcon = "<div class='glyphicon glyphicon-remove ml-5 mt-2' style='font-size: 20px;'></div>"
 
+	$("#all").on("click", function () {
+		location.reload();
+	})
 
-$(document).ready(function(){
+	for (var i = 0; i < channels.length; i++) {
+		$.getJSON('https://wind-bow.glitch.me/twitch-api/users/' + channels[i], function (response) {
+			var channelNames = response._links.self.split('/').pop();
+			var logo = response.logo
+			$.getJSON('https://wind-bow.glitch.me/twitch-api/streams/' + channelNames, function (data) {
+				if (data.stream === null) {
+					offline = (channelNames, logo) => $("#channelsDiv").append(
+						"<div class='row offlineContent' style='border: 1px solid darkgray; background-color: #F1F1F1;'>" +
+						"<div class='col-sm-4'><img class='userIcon my-1' src=" + logo + "></div>" +
+						"<div class='col-sm-4'><h2 class='mt-1'>" + channelNames + "</h2></div>" +
+						"<div class='col-sm-4'>" + offlineIcon + "</div>" +
+						"</div>"
+					)
+					offline(channelNames, logo);
 
+					$('#online').on("click", function () {
+						$('.offlineContent').css('display','none')
+					})
+					$('#offline').on("click", function () {
+						$('.offlineContent').css('display','inherit')
+					})
+				}
+				else if (data.stream != null) {
+					var status = data.stream.channel.status
+					online = (channelNames, logo) => $("#channelsDiv").append(
+						"<div class='row' style='border: 1px solid darkgray; background-color: #F1F1F1;'>" +
+						"<div class='col-sm-4'><img class='userIcon my-1' src=" + logo + "></div>" +
+						"<div class='col-sm-4'><h2 class='mt-1'>" + channelNames + "</h2></div>" +
+						"<div class='col-sm-4'>" + onlineIcon + "</div>" +
+						"<div class='col='sol-sm-12 text-left'><p>"+status+"</p><div>"+
+						"</div>"
+					)
+					online(channelNames, logo);
+					
+					$('#offline').on("click", function () {
+						$('.onlineContent').css('display','none')
+					})
+					$('#online').on("click", function () {
+						$('.onlineContent').css('display','inherit')
+					})
+				}
 
+			});
+		});
+	}
 
-  $("#beginBtn").on("click", function(){
-  
-  
-  $("#start").css('display','none')
-  
-  $("#content").css('display','inherit')
-  
-  $('body').css('background','#F9F9F9')
-  
-  var promise = document.querySelector('video').play();
-  
-  if (promise !== undefined) {
-    promise.then(_ => {
-      // Autoplay started!
-    }).catch(error => {
-      // Autoplay was prevented.
-      // Show a "Play" button so that user can start playback.
-    });
-  }
-  
-  
-  
-  })
-  
-  
-  })
+})
